394e8230315bdcacc949416a799cedef3d8d7057
Brian Kimble <brian@stickbuilt.us>
2025-05-27 14:39:33 +0000

    favicon code added

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -16,6 +16,14 @@ const geistMono = Geist_Mono({
 export const metadata: Metadata = {
   title: 'Battle Board',
   description: 'A digital battlemap for your TTRPGs',
+  icons: {
+    icon: [
+      { url: '/favicon.ico', type: 'image/x-icon' },
+      { url: '/android-chrome-192x192.png', type: 'image/png', sizes: '192x192' },
+      { url: '/android-chrome-512x512.png', type: 'image/png', sizes: '512x512' },
+    ],
+    apple: '/apple-touch-icon.png',
+  },
 };
 
 export default function RootLayout({
